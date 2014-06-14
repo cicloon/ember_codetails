a05847ed3584ef68dc1e17cadfcb426213408fd3
Alejandro <aleon.prof@gmail.com>
2014-06-14 13:21:35 +0200

    added resfull adapter

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,1 +1,10 @@
-EmberCodetails.ApplicationAdapter = DS.FixtureAdapter;
+EmberCodetails.ApplicationAdapter = DS.RESTAdapter.extend({
+  host: 'http://localhost:3000',
+
+  ajax: function(url, method, hash) {
+    hash = hash || {}; // hash may be undefined
+    hash.dataType = 'jsonp'
+    return this._super(url, method, hash);
+  }
+
+});
