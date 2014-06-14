acf6b38c28d89391c16ec8b09b1116874fd9a5f1
Alejandro <aleon.prof@gmail.com>
2014-06-14 10:58:23 +0200

    model data moved to fixtures

--- app/scripts/models/codetail.js
+++ app/scripts/models/codetail.js
@@ -2,3 +2,9 @@ EmberCodetails.Codetail =  DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string')
 });
+
+EmberCodetails.Codetail.FIXTURES = [
+  {id: 1, title: 'Codetail wadus', description: 'Codetail wadus description'},
+  {id: 2, title: 'Codetail foo', description: 'Codetail foo description'},
+  {id: 3, title: 'Codetail bar', description: 'Codetail bar description'}
+];
--- app/scripts/routes/codetails_route.js
+++ app/scripts/routes/codetails_route.js
@@ -1,9 +1,5 @@
 EmberCodetails.CodetailsRoute = Ember.Route.extend({
   model: function(){
-    return [
-      {title: 'Codetail wadus', description: 'Codetail wadus description' },
-      {title: 'Codetail foo', description: 'Codetail foo description'},
-      {title: 'Codetail bar', description: 'Codetail bar description'}
-    ]
+    return this.store.findAll('codetail');
   }
 })
